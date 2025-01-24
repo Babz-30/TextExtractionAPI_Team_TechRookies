28b9f6f7b22a4082a19c1fd8255a925a2a5a8f37
Babitha_30 <babitha.nadar.eu@gmail.com>
2025-01-24 22:35:38 +0100

    Fixing warning

--- POC/ConsoleApp2/ConsoleApp2/TextExtraction.cs
+++ POC/ConsoleApp2/ConsoleApp2/TextExtraction.cs
@@ -5,36 +5,29 @@ namespace OCRApplication
     internal class TextExtraction
     {
         // Path to the trained tessdata folder
-        string tessDataPath = @"path_to_tessdata";
-        public void extractText(string imagePath, string language)
+        readonly string tessDataPath = @"path_to_tessdata";
+        public void ExtractText(string imagePath, string language)
         {
             try
             {
                 // Initialize the Tesseract engine
-                using (var ocrEngine = new TesseractEngine(tessDataPath, language, EngineMode.Default))
-                {
-                    Console.WriteLine("Tesseract engine initialized.");
+                using var ocrEngine = new TesseractEngine(tessDataPath, language, EngineMode.Default);
+                Console.WriteLine("Tesseract engine initialized.");
 
-                    // Load the image file
-                    using (var image = Pix.LoadFromFile(imagePath))
-                    {
-                        Console.WriteLine("Image loaded successfully.");
+                // Load the image file
+                using var image = Pix.LoadFromFile(imagePath);
+                Console.WriteLine("Image loaded successfully.");
 
-                        // Perform OCR on the image
-                        using (var page = ocrEngine.Process(image))
-                        {
-                            // Extract and display text
-                            string extractedText = page.GetText();
-                            display(extractedText);
+                // Perform OCR on the image
+                using var page = ocrEngine.Process(image);
+                // Extract and display text
+                string extractedText = page.GetText();
+                Display(extractedText);
 
 
-                            // Display OCR confidence
-                            float confidence = page.GetMeanConfidence();
-                            Console.WriteLine($"Confidence: {confidence * 100:F2}%");
-
-                        }
-                    }
-                }
+                // Display OCR confidence
+                float confidence = page.GetMeanConfidence();
+                Console.WriteLine($"Confidence: {confidence * 100:F2}%");
             }
             catch (Exception ex)
             {
@@ -42,7 +35,7 @@ namespace OCRApplication
             }
         }
 
-        public void display(String extractedText)
+        public static void Display(String extractedText)
         {
             Console.WriteLine("Extracted Text:");
             Console.WriteLine(extractedText);

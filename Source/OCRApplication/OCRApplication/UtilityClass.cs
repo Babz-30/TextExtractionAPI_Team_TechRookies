77347fdbb7c5086ac98c128882f3e8273d41c31c
Babitha_30 <babitha.nadar.eu@gmail.com>
2025-01-25 13:26:26 +0100

    Adding Utility Class for getting file directory and path.

---
+++ Source/OCRApplication/OCRApplication/UtilityClass.cs
@@ -0,0 +1,59 @@
+﻿namespace OCRApplication
+{
+    public static class UtilityClass
+    {
+
+        // Path to input image directory
+        static readonly string inputImageDirectory = @"path_to_image";
+
+        // Path to the trained tessdata folder
+        static readonly string tessDataPath = @"path_to_tessdata";
+        public static string SolutionDirectory()
+        {
+            string currentDirectory = Directory.GetCurrentDirectory();
+
+            DirectoryInfo? parentDirectory = Directory.GetParent(currentDirectory)?.Parent?.Parent;
+
+            string parentFullName = parentDirectory?.FullName ?? "Parent directory does not exist";
+            
+            return parentFullName;
+
+        }
+
+        public static string ImageDirectory()
+        {
+            return Path.Combine(SolutionDirectory(), inputImageDirectory);
+        }
+
+        public static string ImagePath(string filename)
+        {
+            string imagePath = Path.Combine(ImageDirectory(), filename);
+
+            if (!File.Exists(imagePath))
+            {
+                Console.WriteLine("Image not found.");
+            }
+
+            return imagePath;
+        }
+
+        public static string OutputImagePath(string filename)
+        {
+            string imagePath = Path.Combine(ImageDirectory(), filename);
+
+            if (!File.Exists(imagePath))
+            {
+                File.Create(imagePath);
+            }
+
+            return imagePath;
+        }
+
+        public static string TessDataPath()
+        {
+            return Path.Combine(SolutionDirectory(), tessDataPath);
+
+        }
+
+    }
+}

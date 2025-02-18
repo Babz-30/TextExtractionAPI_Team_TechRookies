bfaf98c4955942be228bd680bc4aef4e18585661
Dell <akanksha.ger26@gmail.com>
2025-02-18 14:18:42 +0100

    Junit for Grayscale-1

---
+++ UnitTestProject/OCRApplicationTest/GrayscaleTest.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using OCRApplication.Preprocesssing;
+using System;
+using System.IO;
+
+namespace OCRApplication.Tests
+{
+    [TestFixture]
+    public class GrayscaleTests
+    {
+        private string _testInputPath;
+        private string _testOutputPath;
+
+        [SetUp]
+        public void Setup()
+        {
+            // Set up test image paths
+            _testInputPath = "test_input.jpg";
+            _testOutputPath = "test_output.jpg";
+
+            // Create a dummy input image
+            using (var bitmap = new System.Drawing.Bitmap(10, 10))
+            {
+                for (int y = 0; y < bitmap.Height; y++)
+                {
+                    for (int x = 0; x < bitmap.Width; x++)
+                    {
+                        bitmap.SetPixel(x, y, System.Drawing.Color.FromArgb(x * 10, y * 10, 100));
+                    }
+                }
+                bitmap.Save(_testInputPath);
+            }
+        }
+
+        
+    }
+}
